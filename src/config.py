d29c62980decb65a0a28f1915959c5b87ba08c31
vatsmanu728 <vatsmanu728+github@gmail.com>
2026-08-04 10:41:10 +0000

    Reduced save_steps from 50 to 10 (save checkpoints more frequently)

--- src/config.py
+++ src/config.py
@@ -121,7 +121,7 @@ def get_config() -> dict:
         "weight_decay": 0.0,
         "lr_scheduler_type": "cosine",
         "logging_steps": 10,
-        "save_strategy": "epoch",    # use save_strategy="steps", save_steps=50, if quota limit
+        "save_strategy": "epoch",    # use save_strategy="steps", save_steps=10 or50, if quota limit
         "eval_strategy": "epoch",
         "save_total_limit": 2,
         ############## "bf16": True,
